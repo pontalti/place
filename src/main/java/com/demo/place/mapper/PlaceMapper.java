bf413f79bdfaf7e27d240bba5ea41d52dc7d86e6
Gustavo Pontalti <gustavopontalti@msn.com>
2025-11-16 20:57:49 +0100

    fix mapper

--- src/main/java/com/demo/place/mapper/PlaceMapper.java
+++ src/main/java/com/demo/place/mapper/PlaceMapper.java
@@ -12,23 +12,16 @@ public interface PlaceMapper {
 
     PlaceRecord toRecord(Place entity);
 
-    @InheritInverseConfiguration
-    Place toEntity(PlaceRecord record);
-
-
-    @Mapping(target = "place", ignore = true)
-    DayOpening toEntity(DayOpeningRecord record);
-
     DayOpeningRecord toRecord(DayOpening entity);
 
+    Place toEntity(PlaceRecord record);
+
+    DayOpening toEntity(DayOpeningRecord record);
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "id", ignore = true)
     void updateEntityFromPatch(PlacePatchRecord patch, @MappingTarget Place entity);
 
-
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "place", ignore = true)
     void updateDayFromPatch(DayOpeningRecord src, @MappingTarget DayOpening tgt);
 
 }
